49e91fbd7dd1bedc4a92828af5370f2cbb4c43df
rana2020701 <ranawagdi4@gmail.com>
2021-02-24 03:03:24 +0200

    passing data

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,12 +17,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(express.static("views/partials"));
 
+var posts =[];
+
 app.get('/', function(req, res){
-  res.render("home", {StartingContent: homeStartingContent})
+  res.render("home", {StartingContent: homeStartingContent, posts: posts})
+
 })
 
 app.get('/about', function(req, res){
   res.render("about", {Acontent: aboutContent})
+ 
 })
 app.get("/contact", function(req, res){
   res.render("contact", {contact:contactContent})
@@ -33,13 +38,12 @@ app.get("/compose", function(req, res){
 
 app.post('/compose', function(req, res){
 
-  var post = {
+  const post = {
     title: req.body.postTitle,
    content: req.body.postBody
   } 
-  
-  console.log(item);
-  res.render("compose")
+  posts.push(post)
+res.redirect("/");
 })
 
 
